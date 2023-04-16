0530419133d900d1b08847d6f8ab710c84d2e6b1
Bowarc <yuyeaaaahhhh@laposte.net>
2023-04-16 12:29:44 +0200

    More lualine config

--- lua/configs/lualine.lua
+++ lua/configs/lualine.lua
@@ -3,28 +3,29 @@
 
 -- stylua: ignore
 local colors = {
-  blue   = '#80a0ff',
-  cyan   = '#79dac8',
-  black  = '#080808',
-  white  = '#c6c6c6',
-  red    = '#ff5189',
-  violet = '#d183e8',
-  grey   = '#303030',
+  blue        = '#80a0ff',
+  cyan        = '#79dac8',
+  black       = '#080808',
+  white       = '#c6c6c6',
+  red         = '#ff5189',
+  violet      = '#d183e8',
+  grey        = '#303030',
+  transparent = "#FF",
 }
 
 local bubbles_theme = {
   normal = {
     a = { fg = colors.black, bg = colors.violet },
     b = { fg = colors.white, bg = colors.grey },
-    c = { fg = colors.black, bg = colors.black },
+    c = { fg = colors.black, bg = colors.transparent },
   },
   insert = { a = { fg = colors.black, bg = colors.blue } },
   visual = { a = { fg = colors.black, bg = colors.cyan } },
   replace = { a = { fg = colors.black, bg = colors.red } },
   inactive = {
-    a = { fg = colors.white, bg = colors.black },
-    b = { fg = colors.white, bg = colors.black },
-    c = { fg = colors.black, bg = colors.black },
+    a = { fg = colors.white, bg = colors.transparent },
+    b = { fg = colors.white, bg = colors.transparent },
+    c = { fg = colors.black, bg = colors.transparent },
   },
 }
 
@@ -38,10 +39,11 @@ require('lualine').setup {
     lualine_a = {
       { 'mode', separator = { left = '' }, right_padding = 2 },
     },
-    lualine_b = { 'filename', 'branch' },
-    lualine_c = { 'fileformat' },
+    -- "buffers" is interesting but i already added something like it at the top of the editor
+    lualine_b = { 'filename', 'branch', 'fileformat' },
+    lualine_c = {},
     lualine_x = {},
-    lualine_y = { 'filetype', 'progress' },
+    lualine_y = { 'filetype', 'diagnostics', 'diff', 'progress' },
     lualine_z = {
       { 'location', separator = { right = '' }, left_padding = 2 },
     },
@@ -52,7 +54,7 @@ require('lualine').setup {
     lualine_c = {},
     lualine_x = {},
     lualine_y = {},
-    lualine_z = { 'location' },
+    lualine_z = {},
   },
   tabline = {},
   extensions = {},
